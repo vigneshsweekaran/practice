73865be5cc10fe86e39ce114ca7020c39d528ead
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2024-01-21 21:53:58 +0800

    payment summary interactive

--- javascript/supersimpledev/javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript/supersimpledev/javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -9,6 +9,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -125,6 +126,7 @@ export function renderOrderSummary() {
         const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove()
         updateCartQuantity();
+        renderPaymentSummary();
       })
     })
 
@@ -172,6 +174,7 @@ export function renderOrderSummary() {
       quantityLabel.innerHTML = newQuantity;
 
       updateCartQuantity();
+      renderPaymentSummary();
     });
   });
 
@@ -181,6 +184,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
